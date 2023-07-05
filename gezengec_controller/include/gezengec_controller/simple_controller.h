cd6e06918e9fc662d6349e813ee95506713c0bbb
yalin demir <yalindemir@outlook.de>
2023-07-05 21:31:39 +0200

    implementing odometry

--- gezengec_controller/include/gezengec_controller/simple_controller.h
+++ gezengec_controller/include/gezengec_controller/simple_controller.h
@@ -4,6 +4,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <Eigen/Core>
+#include <sensor_msgs/JointState.h>
 
 
 class SimpleController
@@ -13,13 +14,22 @@ public:
 
 private:
     void velCallback(const geometry_msgs::Twist &);
+    void jointCallback(const sensor_msgs::JointState &);
 
     ros::NodeHandle nh_;
     ros::Subscriber vel_sub_;
+    ros::Subscriber joint_sub_;
     ros::Publisher right_cmd_pub_;
     ros::Publisher left_cmd_pub_;
 
     // Differential Kinematics
     Eigen::Matrix2d speed_conversion_;
+
+    double wheel_radius_;
+    double wheel_seperation_;
+    double left_wheel_prev_pos_;
+    double right_wheel_prev_pos_;
+    ros::Time prev_time_;
+    
 };
 #endif 
--- gezengec_controller/src/simple_controller.cpp
+++ gezengec_controller/src/simple_controller.cpp
@@ -6,14 +6,21 @@
 SimpleController::SimpleController(const ros::NodeHandle &nh,
                                    double radius,
                                    double separation)
-                                  : nh_(nh)
+                                  : nh_(nh),
+                                  wheel_radius_(radius),
+                                  wheel_seperation_(separation),
+                                  left_wheel_prev_pos_(0.0),
+                                  right_wheel_prev_pos_(0.0)
 {
     ROS_INFO_STREAM("Using wheel radius " << radius);
     ROS_INFO_STREAM("Using wheel separation " << separation);
+    
+    prev_time_ = ros::Time::now();
+
     right_cmd_pub_ = nh_.advertise<std_msgs::Float64>("wheel_right_controller/command", 10);
     left_cmd_pub_ = nh_.advertise<std_msgs::Float64>("wheel_left_controller/command", 10);
     vel_sub_ = nh_.subscribe("gezengec_controller/cmd_vel", 1000, &SimpleController::velCallback, this);
-
+    joint_sub_ = nh_.subscribe("joint_states", 1000, &SimpleController::jointCallback, this);
     speed_conversion_ << radius/2, radius/2, radius/separation, -radius/separation;
     ROS_INFO_STREAM("The conversion matrix is \n" << speed_conversion_);
 }
@@ -32,4 +39,24 @@ void SimpleController::velCallback(const geometry_msgs::Twist &msg)
 
     right_cmd_pub_.publish(right_speed);
     left_cmd_pub_.publish(left_speed);
+}
+
+void SimpleController::jointCallback(const sensor_msgs::JointState &state)
+{
+    double dp_left = state.position.at(0) - left_wheel_prev_pos_;
+    double dp_right = state.position.at(1) - right_wheel_prev_pos_;
+    double dt = (state.header.stamp - prev_time_).toSec();
+
+    left_wheel_prev_pos_ = state.position.at(0);
+    right_wheel_prev_pos_ = state.position.at(1);
+    prev_time_ = state.header.stamp;
+
+    double fi_left = dp_left / dt;
+    double fi_right = dp_right / dt;
+
+    double linear = (wheel_radius_ * fi_right + wheel_radius_* fi_left) / 2;
+    double angular = (wheel_radius_ * fi_right - wheel_radius_* fi_left) / wheel_seperation_;
+
+    ROS_INFO_STREAM("linear: " << linear << " angular: " << angular);
+
 }
