cd197cec332eb10f525d5a95fa0acc00ca5380f8
Your Name <you@example.com>
2023-07-20 18:30:21 +0200

    creating header file for kalman filter

--- gezengec_localization/include/gezengec_localization/kalman_filter.h
+++ gezengec_localization/include/gezengec_localization/kalman_filter.h
@@ -1,8 +1,30 @@
 
 #include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/Imu.h>
+
 
 class KalmanFIilter
 {
 public:
     KalmanFIilter(const ros::NodeHandle &);
+
+private:
+    ros::NodeHandle nh_;
+    ros::Subscriber odom_sub_;
+    ros::Subscriber imu_sub_;
+    ros::Publisher odom_pub_;
+
+    double mean_;
+    double variance_;
+    double imu_angular_z_;
+    bool is_first_odom_;
+    double last_angular_z_;
+    double motion_;
+
+    nav_msgs::Odometry kalman_odom_;
+
+    void odomCallback(const nav_msgs::Odometry& odom_msg);
+    void imuCallback(const sensor_msgs::Imu& imu_msg);
+
 };
